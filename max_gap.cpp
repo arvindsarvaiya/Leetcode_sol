8b0f36aa6eedae8ce0a3e31b431eb14795c31687
arvindsarvaiya <141309078+arvindsarvaiya@users.noreply.github.com>
2025-11-14 14:56:51 +0530

    Create max_gap.cpp

---
+++ max_gap.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 2) return 0;
+
+        int minVal = *min_element(nums.begin(), nums.end());
+        int maxVal = *max_element(nums.begin(), nums.end());
+        if (minVal == maxVal) return 0;
+
+        int bucketSize = max(1, (maxVal - minVal) / (n - 1));
+        int bucketCount = (maxVal - minVal) / bucketSize + 1;
+
+        vector<int> bucketMin(bucketCount, INT_MAX);
+        vector<int> bucketMax(bucketCount, INT_MIN);
+        vector<bool> bucketUsed(bucketCount, false);
+
+  
+        for (int num : nums) {
+            int idx = (num - minVal) / bucketSize;
+            bucketMin[idx] = min(bucketMin[idx], num);
+            bucketMax[idx] = max(bucketMax[idx], num);
+            bucketUsed[idx] = true;
+        }
+
+        int maxGap = 0;
+        int prevMax = minVal;
+
+   
+        for (int i = 0; i < bucketCount; ++i) {
+            if (!bucketUsed[i]) continue;
+            maxGap = max(maxGap, bucketMin[i] - prevMax);
+            prevMax = bucketMax[i];
+        }
+
+        return maxGap;
+    }
+};
